7c11ae78a500f66ef93897ac7552b0b941222278
Musharraf <shahmusharaf1128@gmail.com>
2025-07-21 05:15:20 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main className='home-page'>
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <h3>Add adventure to your life by joining the #vanlife movement. 
+                Rent the perfect van to make your perfect road trip. 
+                Find your van</h3>
+            <div>
+                <h3>Find your van</h3>
+            </div>
+        </main> 
+    )
+}
+
