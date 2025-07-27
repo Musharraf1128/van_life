eadc298d7572d627ad1adcee5364bc106ebf47a7
Musharraf <shahmusharaf1128@gmail.com>
2025-07-27 07:56:56 +0530

    added VanDeatil.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from './pages/Vans';
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail';
 
 export default function App() {
     return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   ) 
