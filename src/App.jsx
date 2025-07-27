c24c23aaaaca9a174b68fb636d15c64e6b961fe3
Musharraf <shahmusharaf1128@gmail.com>
2025-07-27 13:04:04 +0530

    changed path back to absolute

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,25 @@ import VanDetail from './pages/VanDetail';
 
 export default function App() {
     return (
-      <BrowserRouter>
-        <header>
-          <Link className="site-logo" to="/">#VanLife</Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link> 
-          </nav>
-      </header>
+        <BrowserRouter>
+            <header>
+                <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+                <Link to="/about">About</Link>
+                <Link to="/vans">Vans</Link> 
+            </nav>
+            </header>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
-      </Routes>
-    </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
+                <Route path='/vans/:id' element={<VanDetail />} />
+            </Routes>
+
+            <footer>
+                <p>Copyright reserved &copy;VansLife</p>
+            </footer>
+        </BrowserRouter>
   ) 
 }
